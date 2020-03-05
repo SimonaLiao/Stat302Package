070ac68912a41eabfd250194be9f460e94cc7cea
Simona Liao <zliao2@uw.edu>
2020-03-04 16:01:03 -0800

    data iris

---
+++ R/my_iris.R
@@ -0,0 +1,15 @@
+#' Edgar Anderson's Iris Data
+#'
+#' The famous \code{\link[datasets]{iris}} dataset taken from the
+#' \code{datasets} package to practice adding datasets to our R package
+#'
+#' @format a data frame with 150 observation and 5 variables:
+#' \describe{
+#' \item{Sepal.Length}
+#' }
+#'
+#' @source
+#' Fisher, R. A. (1936)
+#'
+#'
+"my_iris"
