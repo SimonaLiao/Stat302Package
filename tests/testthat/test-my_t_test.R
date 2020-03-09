8cdc902ddd5376d4cfdc8e3f700a73374db3032c
Simona Liao <zliao2@uw.edu>
2020-03-09 16:16:07 -0700

    tutorial-mycoef

--- tests/testthat/test-my_t_test.R
+++ tests/testthat/test-my_t_test.R
@@ -1,5 +1,5 @@
 # within test-my_t_test.R
 test_that("my_t_test outputs a list", {
-  expect_type(my_t_test(c(1, 2, 3, 4, 5), mu = 3, alternative = "greater"), list)
+  expect_is(my_t_test(c(1, 2, 3, 4, 5), mu = 3, alternative = "greater"), "list")
 })
 
